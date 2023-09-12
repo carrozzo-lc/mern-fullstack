f9cb4c824720bbd109302da67f9cb777b3511571
Luca Carrozzo <carrozzo.lc@gmail.com>
2023-09-12 17:03:17 +0200

    setup node express app

---
+++ node-express/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/user', (req, res, next) => {
+  res.send(`<h1>User: ${req.body.username}</h1>`);
+});
+
+app.get('/', (req, res, next) => {
+  res.send('<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create user</button></form>')
+});
+
+app.listen(5001);
